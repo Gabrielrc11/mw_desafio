00a567dce1d537d35a41402d7423ca9da45a8ad6
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-10-18 18:49:27 -0300

    refactor: clean code

--- desafio.py
+++ desafio.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from questdb.ingress import Sender
-import requests
+import requests as rq
 
-#Criando tabela "dados" a partir de csv
 tabela = pd.read_csv("questdb-usuarios-dataset.csv")
 df = pd.DataFrame(data=tabela)
 df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
@@ -11,46 +10,40 @@ conf = f'http::addr=questdb:9000;'
 with Sender.from_conf(conf) as sender:
     sender.dataframe(df, table_name='dados', at='timestamp')
 
-#Executando comandos SQL e armazenando resultado em csv
 url = "http://questdb:9000/exec"
 
-#Clientes online por cidade
 params = {
     "query": "SELECT cidadeCliente, COUNT(*) AS total_clientes FROM dados WHERE statusCliente = 1 ORDER BY total_clientes DESC;"
 }
-response = requests.get(url, params=params)
+response = rq.get(url, params=params)
 resultado = pd.DataFrame(response.json()['dataset'], columns=['Cidade', 'Clientes'])
 resultado.to_csv('./resultados/online.csv', index=False, header=True)
 
-#Clientes offline por cidade
 params = {
     "query": "SELECT cidadeCliente, COUNT(*) AS total_clientes FROM dados WHERE statusCliente = 0 ORDER BY total_clientes DESC;"
 }
-response = requests.get(url, params=params)
+response = rq.get(url, params=params)
 resultado = pd.DataFrame(response.json()['dataset'], columns=['Cidade', 'Clientes'])
 resultado.to_csv('./resultados/offline.csv', index=False, header=True)
 
-#Clientes por plano
 params = {
     "query": "SELECT planoContrato, COUNT(*) AS total_clientes FROM dados ORDER BY total_clientes DESC LIMIT 10;"
 }
-response = requests.get(url, params=params)
+response = rq.get(url, params=params)
 resultado = pd.DataFrame(response.json()['dataset'], columns=['Plano', 'Clientes'])
 resultado.to_csv('./resultados/plano.csv', index=False, header=True)
 
-#Clientes por status
 params = {
     "query": "SELECT statusCliente, COUNT(*) AS total_clientes FROM dados ORDER BY total_clientes DESC;"
 }
-response = requests.get(url, params=params)
+response = rq.get(url, params=params)
 resultado = pd.DataFrame(response.json()['dataset'], columns=['Status', 'Clientes'])
 resultado['Status'] = resultado['Status'].replace({0: 'Offline', 1: 'Online'})
 resultado.to_csv('./resultados/status.csv', index=False, header=True)
 
-#Clientes por valor do plano
 params = {
     "query": "SELECT valorPlano, COUNT(*) AS total_clientes FROM dados ORDER BY total_clientes DESC;"
 }
-response = requests.get(url, params=params)
+response = rq.get(url, params=params)
 resultado = pd.DataFrame(response.json()['dataset'], columns=['Valor', 'Clientes'])
 resultado.to_csv('./resultados/valor.csv', index=False, header=True)
