ca860bc9a7f9db83ca92c354d303ce5175ede578
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-10-18 11:07:17 -0300

    feat: questdb connect and sender csv

--- desafio.py
+++ desafio.py
@@ -5,6 +5,7 @@ tabela = pd.read_csv("questdb-usuarios-dataset.csv")
 df = pd.DataFrame(data=tabela)
 df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
 
-conf = f'http::addr=localhost:9000;'
+conf = f'http::addr=questdb:9000;'
 with Sender.from_conf(conf) as sender:
-    sender.dataframe(df, table_name='questdb-usuarios-dataset', at='timestamp')
+    sender.dataframe(df, table_name='dados', at='timestamp')
+    
